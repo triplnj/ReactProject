91068b73189da2bba9f0e5cea318136c25291535
Dejan <ivkovicd69@gmail.com>
2021-12-28 19:28:10 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My name is Dejan Ivkovic and I am a programmer.</h1>
+    <h2>The skill list of mine includes: </h2>
+    <ul>
+      <li style={{ color: "#ac45" }}>JavaScript</li>
+      <li>Nodejs</li>
+      <li>React</li>
+      <li>HTML, CSS</li>
+      <li>MongoDB, Mongoose</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
